9010bc1f422a372518768a3c793318c3b6569606
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2024-06-28 13:30:34 +1200

    Support For mp2024_01
    - Added mp2024_01 files
    - Added gen9_exclusive_assets_peds.meta file
    - Changed __resource.lua to fxmanifest.lua

--- mpstubcloths/__resource.lua
+++
@@ -1,4 +0,0 @@
----- A huge thanks to Alex106 over at gta5-mods.com ----
----- Files have been renamed to work with FiveM ----
----- Courtesy of TayMcKenzieNZ ----
----- Enjoy =) ----
---
+++ mpstubcloths/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TayMcKenzieNZ'
+description 'Resource to stream gen9_exclusive_assets_peds.meta'
+version '1.0.0'
+
+files {
+    'gen9_exclusive_assets_peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'gen9_exclusive_assets_peds.meta'
+
+
+
+---- A huge thanks to Alex106 over at gta5-mods.com ----
+---- Files have been renamed to work with FiveM ----
+---- Courtesy of TayMcKenzieNZ ----
+---- Enjoy =) ----
